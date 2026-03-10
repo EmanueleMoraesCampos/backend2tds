23ceb8c7f9c218443d0a09a56b26b3604df270d7
EmanueleMoraesCampos <campos.emanuele@escola.pr.gov.br>
2026-03-10 11:41:23 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Enderoço Completo");
+let rua = "Rua das flores";
+let numero = 489;
+let cidade = "Curitiba"
+
+let endereco = rua + " , " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereco correto é:" + endereco)
