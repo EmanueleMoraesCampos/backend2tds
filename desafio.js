4cf0c32d23d43a33057f9d2cccd5e307e05d1b01
EmanueleMoraesCampos <campos.emanuele@escola.pr.gov.br>
2026-05-20 13:56:52 +0000

    versão2

--- desafio.js
+++ desafio.js
@@ -1,6 +1,6 @@
 let nomeAluno = "Emanuele";
 let idadeAluno = 17;
 let turmaAluno = "2º Ano TDS";
-let gmail = emanueleescola123.gmail.com;
-let cursos = robotica;
-console.log(`o aluno ${aluno }`)
+let gmail = "emanueleescola123.gmail.com";
+let curso = "robotica";
+console.log(`o aluno ${nomeAluno} tem ${idadeAluno} anos, ela faz o curso de ${curso} e esta na ${turmaAluno}`)
