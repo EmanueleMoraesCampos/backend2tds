210a6c04bf92a1d48afb316676b685c8977a755e
EmanueleMoraesCampos <campos.emanuele@escola.pr.gov.br>
2026-06-01 13:59:47 +0000

    v2

--- desafio.js
+++ desafio.js
@@ -4,3 +4,22 @@ let turmaAluno = "2º Ano TDS";
 let gmail = "emanueleescola123.gmail.com";
 let curso = "robotica";
 console.log(`o aluno ${nomeAluno} tem ${idadeAluno} anos, ela faz o curso de ${curso} e esta na ${turmaAluno}`)
+
+let trabalhoNoMeioDoAno = 1.5
+let provaNoInicioDoAno = (3.0 + 4.0);
+let notasInicioDoAno = 7.0;
+
+let trabalhoNoFinalDoAno = 1.0;
+let provaNoFinalDoAno = (3.1)
+let notasFinalDoAno = 9.0;
+
+let notaTotal = (notasInicioDoAno + notasDoMeioDoAno  + notasFinalDoAno);
+
+let difencaNota = (notasFinalDoAno - notasDoMeioDoAno);
+let notasDeclaradas = "A maior nota dela foi no final do ano de 91 e a menor nota dela foi 70, a diferença de nota foi:";
+
+let frequencia = 94;
+
+console.log(`O aluno ${nomeAluno} tem ${idadeAluno} anos, ela faz o curso de ${curso} e a turma dela é ${turmaAluno} `);
+console.log(`${notasDeclaradas} ${difencaNota}`);
+console.log(`ela teve a frequência de $ {frequência} e teve a media de $ {notaTotal}`);
