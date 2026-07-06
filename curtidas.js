648ec6537afd991864a0e6545ae8ddf31f3e7930
EmanueleMoraesCampos <campos.emanuele@escola.pr.gov.br>
2026-07-06 13:53:58 +0000

    v2

--- curtidas.js
+++ curtidas.js
@@ -1,9 +1,8 @@
-const feedJSON = require('.feed.json');
+const feedJSON = require('./feed.json');
 
 //adicionar uma curtida ao post pelo id
 function curtir(feed,id) {
-const post = feed[0].id == id ? feed[0]
-}
+const post = feed.find(item => item.id === id);
 
 if(!post) {
 console.log("Post não encontrado.");
@@ -15,4 +14,21 @@ console.log("Post não encontrado.");
 return;
 }
  post.metricas.curtidas++;
- console.log('Post de @${post.autor} agora tem $ {}post.metricas.curtidas.')
+ console.log(`Post de @${post.autor} agora tem ${post.metricas.curtidas}.`)
+}
+
+//Remove uma curtida não deixa ir abaixo de zero
+function descurtir (feed, id) {
+ const post = feed.find(item => item.id === id);
+
+ if(post.metricas.curtidas === 0) {
+ console.log("Este post já está com curtidas.");
+ return;
+ }
+
+ post.metricas.curtidas--;
+ console.log(`Curtida removida. Total ${post.metricas.curtidas}`);
+}
+
+curtir(feedJSON, 1);
+descurtir(feedJSON, 1);
